e0328bf56b8a41365bfecbc6c9a5db44cf1ec2f0
trevorb1 <trevor_barnes@sfu.ca>
2025-01-25 16:47:06 -0800

    extractor update

--- analysis/extract_results.py
+++ analysis/extract_results.py
@@ -11,9 +11,8 @@ import pypsa
 from pathlib import Path
 import shutil
 
-NETWORK_DATA = "./data/networks/"
-PROCESSED_DATA = "./data/processed/"
-
+NETWORK_DATA = "./data/networks"
+PROCESSED_DATA = "./data/processed"
 
 def save_results(n: pypsa.Network, scenario: str) -> None:
     # create root scenario folder
@@ -44,31 +43,40 @@ def save_results(n: pypsa.Network, scenario: str) -> None:
 
 
 if __name__ == "__main__":
-    network_data = Path(NETWORK_DATA)
+    network_path = Path(NETWORK_DATA)
 
-    for scenario_dir in network_data.iterdir():
-        scenario = scenario_dir.stem
-        if scenario_dir.is_dir():
-            # check only one interconnect
-            num_interconnects = sum(
-                1 for item in scenario_dir.iterdir() if item.is_dir()
-            )
-            assert num_interconnects == 1, (
-                f"{num_interconnects} interconnects in {scenario_dir}"
-            )
-            for interconnect_dir in scenario_dir.iterdir():
-                # check correct interconnect name
-                assert interconnect_dir.stem in ["western", "eastern", "texas", "usa"]
-                # check only one network file
-                network_dir = Path(interconnect_dir, "networks")
-                num_networks = sum(
-                    1 for item in network_dir.iterdir() if item.is_file()
+    for method in ("static", "dynamic"):
+        network_data = Path(network_path, method)
+        for scenario_dir in network_data.iterdir():
+            scenario = scenario_dir.stem
+            if scenario_dir.is_dir():
+                # check only one interconnect
+                num_interconnects = sum(
+                    1 for item in scenario_dir.iterdir() if item.is_dir()
                 )
-                assert num_networks == 1, f"{num_networks} networks in {network_dir}"
-                for network in network_dir.iterdir():
-                    # check correct file extension
-                    assert network.suffix == ".nc"
-                    # generate intermediate results
-                    print(f"reading {str(network)}")
-                    n = pypsa.Network(str(network))
-                    save_results(n, scenario)
+                assert num_interconnects == 1, (
+                    f"{num_interconnects} interconnects in {scenario_dir}"
+                )
+                for interconnect_dir in scenario_dir.iterdir():
+                    # check correct interconnect name
+                    assert interconnect_dir.stem in [
+                        "western",
+                        "eastern",
+                        "texas",
+                        "usa",
+                    ]
+                    # check only one network file
+                    network_dir = Path(interconnect_dir, "networks")
+                    num_networks = sum(
+                        1 for item in network_dir.iterdir() if item.is_file()
+                    )
+                    assert num_networks == 1, (
+                        f"{num_networks} networks in {network_dir}"
+                    )
+                    for network in network_dir.iterdir():
+                        # check correct file extension
+                        assert network.suffix == ".nc"
+                        # generate intermediate results
+                        print(f"reading {str(network)}")
+                        n = pypsa.Network(str(network))
+                        save_results(n, scenario)
